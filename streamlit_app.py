56913c9139531027a075066343bee14f06f98856
PaulManohar3 <157360568+PaulManohar3@users.noreply.github.com>
2024-01-23 16:01:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 #importing new py package
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 #to normalize the json file we are using json_normalize function  
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
